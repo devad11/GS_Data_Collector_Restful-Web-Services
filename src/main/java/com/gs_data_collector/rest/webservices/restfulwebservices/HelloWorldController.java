7599b492510e79129983289b6fb32d2e10feba3d
Adam Devenyi <adam.devenyi@mycit.ie>
2020-01-29 14:36:37 +0000

    pathVar

--- src/main/java/com/gs_data_collector/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/gs_data_collector/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,14 +1,24 @@
 package com.gs_data_collector.rest.webservices.restfulwebservices;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
-	@RequestMapping(method=RequestMethod.GET, path = "/hello-world")	
+	@GetMapping(path = "/hello-world")	
 	public String helloWorld() {
 		return "Hello World";
+	}
+	
+	@GetMapping(path = "/hello-world-bean")	
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World");
+	}
+	
+	@GetMapping(path = "/hello-world/path-variable/{name}")	
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World, %s", name));
 	}
 }
