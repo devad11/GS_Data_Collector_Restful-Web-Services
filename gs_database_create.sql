51bad8df4e973619a3e692f1c217259139c55df6
Adam Devenyi <adam.devenyi@mycit.ie>
2020-02-18 11:30:17 +0000

    DB setup

---
+++ gs_database_create.sql
@@ -0,0 +1,9 @@
+create table hibernate_sequence (next_val bigint) engine=InnoDB
+insert into hibernate_sequence values ( 1 )
+create table todo (id bigint not null, description varchar(255), is_done bit not null, target_date datetime, username varchar(255), primary key (id)) engine=InnoDB
+create table hibernate_sequence (next_val bigint) engine=InnoDB
+insert into hibernate_sequence values ( 1 )
+create table todo (id bigint not null, description varchar(255), is_done bit not null, target_date datetime, username varchar(255), primary key (id)) engine=InnoDB
+create table hibernate_sequence (next_val bigint) engine=InnoDB
+insert into hibernate_sequence values ( 1 )
+create table todo (id bigint not null, description varchar(255), is_done bit not null, target_date datetime, username varchar(255), primary key (id)) engine=InnoDB
